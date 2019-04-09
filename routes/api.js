d93fe2c97e2a518dc6a3511f0005c28d3002c94a
XXXXzy <381924237@qq.com>
2019-04-09 23:13:08 +0800

    初步实现增删改查

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,31 @@ router.get('/notes', function(req, res, next) {
 })
 
 router.post('/notes/add', function(req, res, next) {
-  console.log('/notes/add')
+  let note = req.body.note
+  Note.create({text: note}).then(() => {
+    res.send({status: 0})
+  }).catch(() => {
+    res.send({status: 1, errorMsg: '数据库出错'})
+  })
 })
 
 router.post('/notes/edit', function(req, res, next) {
-  console.log('/notes/edit')
+  let noteId = req.body.id
+  let note = req.body.note
+  Note.update({text: note}, {where: {id: noteId}}).then(() => {
+    res.send({status: 0})
+  }).catch(() => {
+    res.send({status: 1, errorMsg: '数据库异常或者你没有权限'})
+  })
 })
 
 router.post('/notes/delete', function(req, res, next) {
-  console.log('/notes/delete')
+  let noteId = req.body.id
+  Note.destroy({where: {id: noteId}}).then(() => {
+    res.send({status: 0})
+  }).catch(() => {
+    res.send({status: 1, errorMsg: '数据库异常或者你没有权限'})
+  })
 })
 
 
